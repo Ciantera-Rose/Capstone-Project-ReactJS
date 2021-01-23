fd6131ae8f4d1d8eb241e7bb0bdc695aacdbdf3c
Ciantera-Rose <ciantera.rose@gmail.com>
2021-01-22 20:43:36 -0500

    add styles, add start presentational components for profile card

--- src/user/components/UserItem.js
+++ src/user/components/UserItem.js
@@ -1,19 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import UserAvatar from "../../presentational-components/UserAvatar";
 
 const UsersItem = (props) => {
   return (
-    <li className="">
-      <div className="">
-        <div className="">
-          <img src={props.image} alt={props.name} />
-        </div>
-        <div className="">
-          <h2>{props.name}</h2>
-          <h3>
-            {props.locationCount}{" "}
-            {props.locationCount === 1 ? "Location" : "Locations"}
-          </h3>
-        </div>
+    <li className="user-item">
+      <div className="user-item-content">
+        <Link to={`/${props.id}/places`}>
+          <div className="user-item-image">
+            <UserAvatar src={props.image} alt={props.name} />
+          </div>
+          <div className="user-item-info">
+            <h2>{props.name}</h2>
+            <h3>
+              {props.locationCount}
+              {props.locationCount === 1 ? "Location" : "Locations"}
+            </h3>
+          </div>
+        </Link>
       </div>
     </li>
   );
@@ -26,3 +31,4 @@ export default UsersItem;
 // Pass user image div 2 and <img tag/>
 // Pass user info div 3 <h2> users name </h2>
 // Pass # of locations user has created <h3> Location Count <h3> 1 location or many locations
+// Add dynamic link so you can click on user and be taken to places each user created
--- src/user/components/UsersList.js
+++ src/user/components/UsersList.js
@@ -5,14 +5,14 @@ import UserItem from "./UserItem";
 const UsersList = (props) => {
   if (props.items.length === 0) {
     return (
-      <div>
+      <div className="center">
         <h2>No users found</h2>
       </div>
     );
   }
 
   return (
-    <ul>
+    <ul className="users-list">
       {props.items.map((user) => {
         return (
           <UserItem
