9a93ccc14675483af126073ae811fb9e7fe96540
Ciantera-Rose <ciantera.rose@gmail.com>
2021-01-23 21:12:12 -0500

    add initial conditions and logic to map list of user locations

--- src/locations/components/LocationList.js
+++ src/locations/components/LocationList.js
@@ -1,7 +1,40 @@
 import React from "react";
 
+import LocationItem from "./LocationItem";
+
 const LocationList = (props) => {
-  return <div></div>;
+  if (props.items.length === 0) {
+    return (
+      <div className="">
+        <UserCard>
+          <h2>No Locations found. Please add a location</h2>
+          <button>Share Location</button>
+        </UserCard>
+      </div>
+    );
+  }
+
+  return (
+    <ul>
+      {props.items.map((location) => (
+        <Location
+          id={location.id}
+          image={location.imageUrl}
+          title={location.title}
+          description={location.description}
+          address={location.address}
+          userId={location.user}
+          coordinates={location.location}
+        />
+      ))}
+    </ul>
+  );
 };
 
 export default LocationList;
+
+// Will get items prop, array of locations from LocationItem
+// If no locations, return no locations found
+// User <UserCard> like in Users if no places found
+// Button to share location that leads/links to NewLocation Page
+// If locations => return <ul> and map array list of objects to jsx elements
