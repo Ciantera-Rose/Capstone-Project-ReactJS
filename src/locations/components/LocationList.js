a91d2906c869319c57d83184d5e235450eab40a3
Ciantera-Rose <ciantera.rose@gmail.com>
2021-01-23 20:47:57 -0500

    organize and set up user location components

---
+++ src/locations/components/LocationList.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LocationList = (props) => {
+  return <div></div>;
+};
+
+export default LocationList;
---
+++ src/locations/pages/UserLocations.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const UserLocations = () => {
+  return <div></div>;
+};
+
+export default UserLocations;
+
+// Fetch and render all the locations that belong to a user
+// Use mock data until backend built
+// Return list of locations
+// Need a Location list Component to pass to this one
