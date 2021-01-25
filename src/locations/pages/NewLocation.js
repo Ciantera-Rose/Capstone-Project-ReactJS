e3dc25cb47e561d3edd62831ede68eb16524b130
Ciantera-Rose <ciantera.rose@gmail.com>
2021-01-24 20:32:05 -0500

    add useEffect for props and input state change, add inputHandler to New Location, wrap with useCallback to prevent useEffect again, bind handlers to onInput prop

--- src/locations/pages/NewLocation.js
+++ src/locations/pages/NewLocation.js
@@ -1,17 +1,32 @@
-import React from "react";
+import React, { useCallback } from "react";
 
 import Input from "../../shared/components/form/Input";
 import { VALIDATOR_REQUIRE } from "../../shared/components/utility/Validators";
 
 const NewLocation = () => {
+  const titleInputHandler = useCallback((id, value, isValid) => {}, []);
+
+  const descriptionInputHandler = useCallback((id, value, isValid) => {}, []);
+
   return (
     <form className="location-form">
       <Input
+        id="title"
         element="input"
         type="text"
         label="Location Title"
         validators={[VALIDATOR_REQUIRE()]}
-        errorText="Please enter a valid title"
+        errorText="Please enter a valid title."
+        onInput={descriptionInputHandler}
+      />
+      <Input
+        id="description"
+        element="textarea"
+        type="Description"
+        label="Location Title"
+        validators={[VALIDATOR_MIN_LENGTH(5)]}
+        errorText="Please enter at least 5 characters for a valid description."
+        onInput={titleInputHandler}
       />
     </form>
   );
