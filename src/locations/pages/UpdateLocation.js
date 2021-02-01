29c76dc4a572b02eb7b0f64346830ed9060fe121
Ciantera-Rose <ciantera.rose@gmail.com>
2021-02-01 01:58:59 -0500

    add start fetch latest data from backend, add usehttp hook, add useState to manage loaded locations, add useEffect and fetch location function, send req, await res data and pass in url, add try catch, setLoadedData to resData for location

--- src/locations/pages/UpdateLocation.js
+++ src/locations/pages/UpdateLocation.js
@@ -8,40 +8,12 @@ import {
   VALIDATOR_MINLENGTH,
 } from "../../shared/components/utility/Validators";
 import { useForm } from "../../shared/components/hooks/Form-hook";
-
-const MOCK_LOCATIONS = [
-  {
-    id: "l1",
-    title: "The Brooklyn Mirage",
-    description:
-      "The Brooklyn Mirage is a breathtaking open-air sanctuary in the heart of the Avant Gardner complex.",
-    imageUrl:
-      "https://images.squarespace-cdn.com/content/v1/597a8b3920099e0bff668154/1538683215516-2O43UPWH0BHWLTGPF6DE/ke17ZwdGBToddI8pDm48kEZk6F5PbQiC1r1IZ2IoUeR7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QPOohDIaIeljMHgDF5CVlOqpeNLcJ80NK65_fV7S1UfvbNRGeuxQFwQ8dTRP7_IjByFLq5tUM4qN8xXPNmdulg0wU7-gbCzcJVB_xdsPuSg/image-asset.jpeg",
-    address: "140 Stewart Ave, Brooklyn, NY 11237",
-    location: {
-      lat: 40.7108803,
-      lng: -73.9257375,
-    },
-    userId: "u1",
-  },
-  {
-    id: "l2",
-    title: "Output",
-    description:
-      "North Brooklyn's premier electronic and dance music venue that offers a more low-key experience than its bottle-service-heavy competitors, but still delivers heavy-hitting performers",
-    imageUrl:
-      "https://media-cdn.tripadvisor.com/media/photo-s/06/25/b8/2d/output.jpg",
-    address: "74 Wythe Ave, Brooklyn, NY 11249",
-    location: {
-      lat: 40.7222917,
-      lng: -73.9578222,
-    },
-    userId: "u2",
-  },
-];
+import useHttp from "../../shared/components/hooks/Http-hook";
 
 const UpdateLocation = () => {
-  const [isLoading, setIsLoading] = useState(true);
+  const { isLoading, error, sendRequest, clearError } = useHttp();
+  const [loadedLocation, setLoadedLocation] = useState();
+
   const locationId = useParams().locationId;
 
   const [formState, InputHandler, setFormData] = useForm(
@@ -57,7 +29,17 @@ const UpdateLocation = () => {
     },
     false
   );
-  const updatedLocation = MOCK_LOCATIONS.find((l) => l.id === locationId);
+
+  useEffect(() => {
+    const fetchLocation = async () => {
+      try {
+        const responseData = await sendRequest(
+          `http://localhost:5000/api/locations/${locationId}`
+        );
+        setLoadedLocation(responseData.location);
+      } catch (err) {}
+    };
+  }, [sendRequest, locationId]);
 
   useEffect(() => {
     if (updatedLocation) {
