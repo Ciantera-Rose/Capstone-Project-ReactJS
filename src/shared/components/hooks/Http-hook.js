4830149b52b98c3c145fb880ebb887a2998cc675
Ciantera-Rose <ciantera.rose@gmail.com>
2021-01-31 20:01:06 -0500

    add manage loading state before and after, return res data, return isLoading, error, sendreq obj, wrap in useCallback so function doesn’t get recreated when component using hook rerenders, add clear err to call error func for hook

--- src/shared/components/hooks/Http-hook.js
+++ src/shared/components/hooks/Http-hook.js
@@ -1,28 +1,38 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 
 const useHttp = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState();
 
-  const sendRequest = async (
-    url,
-    method = "GET",
-    body = null,
-    headers = {}
-  ) => {
-    try {
-      const response = await fetch(url, {
-        method,
-        body,
-        headers,
-      });
-      const responseData = await response.json();
+  const sendRequest = useCallback(
+    async (url, method = "GET", body = null, headers = {}) => {
+      setIsLoading(true);
+      try {
+        const response = await fetch(url, {
+          method,
+          body,
+          headers,
+        });
+        const responseData = await response.json();
 
-      if (!response.ok) {
-        throw new Error(responseData.message);
+        if (!response.ok) {
+          throw new Error(responseData.message);
+        }
+
+        return responseData;
+      } catch (err) {
+        setError(err.message);
       }
-    }
+      setIsLoading(false);
+    },
+    []
+  );
+
+  const clearError = () => {
+    setError(null);
   };
+
+  return { isLoading, error, sendRequest, clearError };
 };
 
 export default useHttp;
